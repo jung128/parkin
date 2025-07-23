1ca3f3fbf44111fe0552ab2bb671599638d933bf
ssu09090 <iso99jsj@naver.com>
2025-07-23 14:13:53 +0900

    0723 오전 수정

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.scss";
 import Header from "./Components/Header";
 import Banner from "./Components/Banner";
 import Footer from "./Components/Footer";
+import ContentsList from "./Components/ContentsList";
 
 const App = () => {
   return (
     <div>
-
+      <ContentsList />
     </div>
   );
 };
