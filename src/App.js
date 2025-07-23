2908889beb9e8a5b6ffcca92deae69815b5725a5
yull03 <rlaaudtjr67@gmail.com>
2025-07-23 16:37:42 +0900

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import "./App.scss";
 import Header from "./Components/Header";
 import Banner from "./Components/Banner";
 import Footer from "./Components/Footer";
-import ItemPage from "./Components/ItemPage";
 import ContentsList from "./Components/ContentsList";
 
 const App = () => {
   return (
     <div>
-      <ItemPage/>
       <Header />
       <Banner />
       <ContentsList />
