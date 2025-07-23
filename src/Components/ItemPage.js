7184d002d6a1b7fe3040a917f1ba9d617218c76c
yull03 <rlaaudtjr67@gmail.com>
2025-07-23 12:59:39 +0900

    text

--- src/Components/ItemPage.js
+++ src/Components/ItemPage.js
@@ -7,13 +7,14 @@ const ItemPage = () => {
             <Header />
             <div className="top-item">
                 <img src="../image/images/main.jpg" alt="메인 이미지" className="shrink" />
-                <h1>개인 주차장 공유</h1>
+                <h1>개인 주차장 공유 (기능 내용 개인적 수정)</h1>
                 <p>내 주차장, 이제는 공유하세요!<br />
                     개인 주차공간 공유 기능 사용하지 않는 주차 공간, <br />
                     그대로 두시겠어요?<br />
                     당신이 비워두는 그 시간, 누군가는 주차 공간이 절실합니다. <br />
                     우리 앱은 개인 주차장을 시간 단위로 <br />
-                    공유할 수 있는 기능을 제공합니다.</p>
+                    공유할 수 있는 기능을 제공합니다.
+                    (기능 내용 개인적 수정)</p>
             </div>
             <div className="item-list">
                 <h3>상세 설명</h3>
